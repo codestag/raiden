9dc22e1f3db32e7e7954d81d169afd6d3d452f01
Ram Ratan Maurya <ratan.maurya@gmail.com>
2016-07-04 19:24:52 +0530

    Apple post tile styles on pages

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -7,12 +7,26 @@
  * @package Raiden
  */
 
+$thumbnail_url = get_the_post_thumbnail_url();
+
+$post_classes = '';
+if ( has_post_thumbnail() ) {
+	$post_classes .= 'post-tile single-post-cover has-post-thumbnail';
+} else {
+	$post_classes .=  'no-page-cover';
+}
+
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	</header><!-- .entry-header -->
+
+	<div class="<?php echo esc_attr( $post_classes ); ?>" <?php if ( has_post_thumbnail() ) echo 'style="background-image:url(' . esc_url( $thumbnail_url ) . ')"'; ?>>
+		<div class="link-overlay"></div>
+
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</header><!-- .entry-header -->
+	</div>
 
 	<div class="entry-content">
 		<?php
